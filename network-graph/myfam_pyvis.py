8c0dccd768ced5d4ede97e8ce13740c318234d13
stuffbyyuki <kake.19940627@gmail.com>
2022-03-29 13:50:32 -0600

    cleaned a bit

--- network-graph/myfam_pyvis.py
+++ network-graph/myfam_pyvis.py
@@ -2,10 +2,10 @@ from pyvis.network import Network
 import pandas as pd
 import matplotlib.pyplot as plt
 
-got_net = Network(height='750px', width='100%', bgcolor='#222222', font_color='white')
+net = Network(height='750px', width='100%', bgcolor='#222222', font_color='white')
 
 # set the physics layout of the network
-got_net.barnes_hut()
+net.barnes_hut()
 got_data = pd.read_csv('data.csv')
 
 sources = got_data['source']
@@ -19,16 +19,16 @@ for e in edge_data:
     dst = e[1]
     w = e[2]
 
-    got_net.add_node(src, src, title=src)
-    got_net.add_node(dst, dst, title=dst)
-    got_net.add_edge(src, dst, value=w)
+    net.add_node(src, src, title=src)
+    net.add_node(dst, dst, title=dst)
+    net.add_edge(src, dst, value=w)
 
-neighbor_map = got_net.get_adj_list()
+neighbor_map = net.get_adj_list()
 
 # add neighbor data to node hover data
-for node in got_net.nodes:
+for node in net.nodes:
     node['title'] += ' Neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
     node['value'] = len(neighbor_map[node['id']])
 
 plt.show()
-got_net.show('myfamily.html')
+net.show('myfamily.html')
